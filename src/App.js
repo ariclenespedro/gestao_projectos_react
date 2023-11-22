03acf1eddff25ff7f8c76791991f4f8b6c127978
ariclenespedro <pedroariclenes@gmail.com>
2023-11-22 17:01:43 +0100

    Estruturando as rotas do projectos

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
-import Navbar from "./layouts/Navbar";
+import Navbar from "./components/layouts/Navbar";
+
+
+import Container from "./components/layouts/Container";
+
+
+
 
 function App() {
   return (
     <Router>
       <Navbar/>
       <Routes>
-        <Route path="/" exact element={<Home/>} ></Route>
-        <Route path="/company" exact element={ <Company/> } ></Route>
-        <Route path="/newproject" exact element={<NewProject/>} ></Route>
+        <Container>
+          <Route path="/" exact element={<Home/>} ></Route>
+          <Route path="/company" exact element={ <Company/> } ></Route>
+          <Route path="/newproject" exact element={<NewProject/>} ></Route>
+        </Container>
       </Routes>
       <p>Footer</p>
     </Router>
