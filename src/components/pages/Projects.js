9d02f01aef03c12e9200bcf4a7df448f4ab05fe4
Ariclenes Heitor da Silva Pedro <70267430+ariclenespedro@users.noreply.github.com>
2023-12-15 11:28:53 +0100

    Criando Dhasboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,9 @@
 import { useLocation } from 'react-router-dom';
 
 import Message from "../layouts/Message";
+import styles from "../pages/Projects.module.css";
+import Container from "../layouts/Container";
+import LinkButton from "../layouts/LinkButton";
 
 function Projects() {
 
@@ -14,13 +17,21 @@ function Projects() {
     }
 
     return(
-        <div>
-            <h1>Meus Projectos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projectos</h1>
+                <LinkButton to="/newproject" text="Criar projecto" />
+            </div>
             {   
-                message ? 
-                (<Message type={"success"} msg={message} />) :
-                 console.log("Não ha mensagem...")
+                message 
+                ? 
+                    (<Message type={"success"} msg={message} />)
+                : 
+                    console.log("Não ha mensagem...")
             }
+            <Container customClass="start">
+                <p>...Projectos</p>
+            </Container>
             
         </div>
     )
